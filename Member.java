e1e97b44f36adc513e81424dcf2dbfc4cf049cc1
mfathirr <muhdyathir623@gmail.com>
2026-04-04 21:29:21 +0700

    add feature sorting based on duration

--- Member.java
+++ Member.java
@@ -10,7 +10,7 @@ public class Member extends User {
     }
 
     // traversal show semua lagu
-    public void lihatPlaylist(Lagu[] playlist, int jumlahLagu) {
+    public void tampilkanSemuaLagu(Lagu[] playlist, int jumlahLagu) {
 
         if (jumlahLagu == 0) {
             System.out.println("Playlist kosong");
@@ -24,7 +24,7 @@ public class Member extends User {
         }
     }
 
-    //searching pakai linear search
+    // searching pakai linear search
     public void cariLagu(Lagu[] playlist, int jumlahLagu, String judul) {
 
         boolean ditemukan = false;
@@ -42,5 +42,23 @@ public class Member extends User {
         if (!ditemukan) {
             System.out.println("Lagu tidak ditemukan");
         }
+    }
+
+    // Mengurutkan lagu berdasarkan durasi (ASC)
+    // Dimulai dari durasi terpendek
+    public void urutkanLaguBerdasarkanDurasi(Lagu[] playlist, int jumlahLagu) {
+        for (int i = 0; i < jumlahLagu - 1; i++) {
+            for (int j = 0; j < jumlahLagu - i - 1; j++) {
+
+                if (playlist[j].durasiGetter() > playlist[j + 1].durasiGetter()) {
+
+                    Lagu temp = playlist[j];
+                    playlist[j] = playlist[j + 1];
+                    playlist[j + 1] = temp;
+                }
+            }
+        }
+
+        tampilkanSemuaLagu(playlist, jumlahLagu);
     }
 }
--- PlaylistOOP.java
+++ PlaylistOOP.java
@@ -24,22 +24,22 @@ public class PlaylistOOP {
 
         int pilihan = 0;
 
-        while (pilihan != 3) {
+        while (pilihan != 6) {
 
             System.out.println("\n=== MENU PLAYLIST MUSIK ===");
             System.out.println("1. Tampilkan semua lagu");
             System.out.println("2. Tambah lagu baru");
             System.out.println("3. Hapus lagu berdasarkan judul");
             System.out.println("4. Cari lagu berdasarkan judul");
-            System.out.println("5. Keluar");
+            System.out.println("5. Urut lagu berdasarkan durasi");
+            System.out.println("6. Keluar");
             System.out.print("Pilih: ");
             pilihan = input.nextInt();
             input.nextLine();
 
             if (pilihan == 1) {
-                member.lihatPlaylist(playlist, jumlahLagu);
-            } 
-            else if (pilihan == 2) {
+                member.tampilkanSemuaLagu(playlist, jumlahLagu);
+            } else if (pilihan == 2) {
                 String lagi = "y";
 
                 while (lagi.equalsIgnoreCase("y")) {
@@ -50,25 +50,24 @@ public class PlaylistOOP {
                     lagi = input.nextLine();
 
                 }
-            } 
-            else if (pilihan == 3) {
+            } else if (pilihan == 3) {
                 System.out.print("Masukkan judul lagu yang ingin dihapus: ");
                 String judul = input.nextLine();
 
                 jumlahLagu = admin.hapusLagu(playlist, jumlahLagu, judul);
-            } 
-            else if (pilihan == 4) {
+            } else if (pilihan == 4) {
                 System.out.print("Masukkan judul lagu yang dicari: ");
                 String judul = input.nextLine();
 
                 member.cariLagu(playlist, jumlahLagu, judul);
-            } 
-            else if (pilihan == 5) {
+            } else if (pilihan == 5) {
+                member.urutkanLaguBerdasarkanDurasi(playlist, jumlahLagu);
+            } else if (pilihan == 6) {
                 System.out.println("Program selesai");
-            } 
-            else {
+            } else {
                 System.out.println("Pilihan tidak valid");
             }
         }
     }
+
 }
